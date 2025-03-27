4449088101d6edd6ee15b36a9960c63041e88aa0
Draco H <dhuizar@email.neit.edu>
2025-03-27 10:44:24 -0400

    small and large balling

--- level1/js/week1_boundary_detection_bounce.js
+++ level1/js/week1_boundary_detection_bounce.js
@@ -30,18 +30,26 @@ function animate()
 	if(ball.x > canvas.width - ball.width/2)
 	{
 		ball.vx = -ball.vx;	
+		ball.width -= 25;
+		ball.x -= 25;
 	}
 	if(ball.x < 0 + ball.width/2)
 		{
 			ball.vx = -ball.vx;	
+			ball.width += 25;
+			ball.x += 25;
 		}
 	if(ball.y > canvas.height - ball.height/2)
 			{
 				ball.vy = -ball.vy;	
+				ball.width -= 25;
+				ball.y -= 25;
 			}
 	if(ball.y < 0 + ball.height/2)
 				{
 					ball.vy = -ball.vy;	
+					ball.width += 25;
+					ball.y += 25;
 				}
 	//---------------------------------------------------
 	
