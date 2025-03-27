627bcda5a29e1f1f9a6e771bff8c1510b94ed524
Draco H <dhuizar@email.neit.edu>
2025-03-27 16:31:51 -0400

    purple paddle

--- level2/game.js
+++ level2/game.js
@@ -2,9 +2,23 @@ var canvas;
 var context;
 var timer;
 var interval = 1000/60;
+var player;
 
 canvas = document.getElementById("canvas");
-
 context = canvas.getContext("2d");
 
+player = new GameObject();
+player.x = 30;
+player.width = 15;
+player.height = 60;
+player.color = "purple";
 timer = setInterval(animate, interval);
+
+function animate()
+{
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	//player.x += 2;
+
+	player.drawRect();
+}
